0e157449150cab6c498cec843ef4fc5d3bc7c281
annagrl <you@example.com>
2022-04-20 04:30:59 -0300

    top10

---
+++ src/Services/getProductsTop10.js
@@ -0,0 +1,10 @@
+export default async function getProducts(setProducts) {
+
+    try {
+      const products = await fetch('http://18.189.31.114:8080/products%22')
+        .then((response) => response.json());
+      setProducts(products.slice(0,10));
+    } catch (error) {
+      alert("Houve um erro ao comunicar com o servidor");
+    }
+  }
